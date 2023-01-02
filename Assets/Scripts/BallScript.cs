d80ac5f0834b3b186a9f17396569157d86eb8741
Roman Lagunovski <r_lagunovski@twistellar.com>
2023-01-02 16:05:38 +0100

    mend

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     public float Power;
-    // Start is called before the first frame update
+
     void Start()
     {
         GetComponent<Rigidbody>().AddForce(new Vector3(0,0,1) * Power, ForceMode.Impulse);
